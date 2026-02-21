cef90a3c1776b67831f2b03000017b56130ae04d
vitosant <vitor.200483@gmail.com>
2026-02-21 08:34:28 -0300

    tests: add main test

--- src/main.c
+++ src/main.c
@@ -17,28 +17,26 @@ t_minirt meu_teste(void);
 
 int	main(int ac, char **av)
 {
-	// t_minirt	*rt;
-	// int			fd;
+	t_minirt	*rt;
+	int			fd;
 
-	// if (ac != 2)
-	// 	return (panic(ERR_ARG_NUM));
-	// else
-	// {
-	// 	if (!check_filename(av[1]))
-	// 		return (panic(ERR_ARG_INVALID));
-	// 	fd = open(av[1], O_RDONLY);
-	// 	if (fd < 0)
-	// 		return (panic(ERR_SYSCALL));
-	// 	rt = start_minirt(av[1]);
-	// 	parse_minirt(rt, fd);
-	// 	run_rt(rt);
-	// 	if (close(fd) == -1)
-	// 		desperation(rt, ERR_SYSCALL);
-	// }
-	(void) ac;
-	(void) av;
-	t_minirt minis = meu_teste();
-	run_rt(&minis);
+	if (ac != 2)
+		return (panic(ERR_ARG_NUM));
+	else
+	{
+		if (!check_filename(av[1]))
+			return (panic(ERR_ARG_INVALID));
+		fd = open(av[1], O_RDONLY);
+		if (fd < 0)
+			return (panic(ERR_SYSCALL));
+		rt = start_minirt(get_name(av[1]));
+		parse_minirt(rt, fd);
+		if (!gc_addlst(rt->scene->shape, rt->gc, GC_DEFAULT))
+			desperation(rt, ERR_SYSCALL);
+		run_rt(rt);
+		if (close(fd) == -1)
+			desperation(rt, ERR_SYSCALL);
+	}
 	return (SUCCESS);
 }
 
