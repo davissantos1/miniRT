04c7398c61f0bc96c25c1110126deafcd63e9541
vitosant <vitor.200483@gmail.com>
2026-02-28 14:01:30 -0300

    refact(patterns): refact color patterns

---
+++ src/bonus/1_run_rt_bonus/patterns/get_pattern_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_pattern_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 11:41:01 by vitosant          #+#    #+#             */
+/*   Updated: 2026/02/28 11:56:22 by vitosant         ###    ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vec4.h"
+#include "shapes_bonus.h"
+#include "pattern_bonus.h"
+#include "settings.h"
+
+void	get_pattern(t_hit *hit, t_pattern op)
+{
+	static t_patterns	patterns[3] = {checker, striped, ring};
+	t_point				p;
+
+	p = vec4_multi_mtx4(hit->inverse, hit->hit_point);
+	p.x += EPSILON;
+	p.y += EPSILON;
+	p.z += EPSILON;
+	p = vec4_scale(hit->mat.pattern_const, p);
+	hit->mat.color = patterns[op](p, hit->mat);
+	if (!vec4_is_diff(hit->mat.color, hit->mat.color2))
+	{
+		hit->mat.ka = hit->mat.ka2;
+		hit->mat.ks = hit->mat.ks2;
+		hit->mat.kr = hit->mat.kr2;
+	}
+}
---
+++ src/bonus/1_run_rt_bonus/patterns/patterns_bonus.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   patterns_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 11:40:26 by vitosant          #+#    #+#             */
+/*   Updated: 2026/02/28 11:40:27 by vitosant         ###    ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shapes_bonus.h"
+#include "vec4.h"
+#include "pattern_bonus.h"
+#include <math.h>
+
+t_color	striped(t_point p, t_material m)
+{
+	int		x;
+	t_vec4	colors[2];
+
+	colors[0] = m.color;
+	colors[1] = m.color2;
+	x = (int)floor(p.x * 3.0);
+	return (colors[abs(x) % 2]);
+}
+
+t_color	ring(t_point p, t_material m)
+{
+	double		sqrt_x;
+	double		sqrt_z;
+	int			result;
+	t_vec4		colors[2];
+
+	colors[0] = m.color;
+	colors[1] = m.color2;
+	sqrt_z = p.z * p.z * 10;
+	sqrt_x = p.x * p.x * 10;
+	result = (int)floor(sqrt(sqrt_x + sqrt_z));
+	return (colors[result % 2]);
+}
+
+t_color	checker(t_point p, t_material m)
+{
+	int		result;
+	t_vec4	colors[2];
+
+	colors[0] = m.color;
+	colors[1] = m.color2;
+	result = (int)(floor(p.x) + floor(p.y) + floor(p.z));
+	return (colors[abs(result) % 2]);
+}
