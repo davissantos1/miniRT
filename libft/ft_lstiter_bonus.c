7bfe7c35e5455d58d51064ad607d8d2aaa265ad0
Davi Santos <davisimoesdossantos1@gmail.com>
2026-03-08 19:22:32 -0300

    fix: solved ft_lstiter bug

--- libft/ft_lstiter_bonus.c
+++ libft/ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 17:31:35 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/03/08 16:06:06 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/03/08 19:20:37 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 	head = lst;
 	while (head)
 	{
-		f(lst->content);
+		f(head->content);
 		head = head->next;
 	}
 }
